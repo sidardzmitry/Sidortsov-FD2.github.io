002db880ae05bf1e6b2a2beae770b761dd83abdb
sidardzmitry <dmx3214@mail.ru>
2021-12-05 15:35:39 +0300

    gtrgtr

--- ДЗ_DOM/dom.js
+++ ДЗ_DOM/dom.js
@@ -1,5 +1,6 @@
 //добавляем body в переменную...
 const body = document.querySelector("body");
+body.style.backgroundColor = "rgba(0, 0, 0, .3)";
 
 //создаем и добавляем элемент div с классом  и вставляем в body...
 const divBox = document.createElement("div");
@@ -13,6 +14,7 @@ function createInput(name, placeholder) {
   inps.setAttribute("name", name);
   inps.style.width = "350px";
   inps.style.height = "25px";
+  inps.style.margin = "0 4px 3px 0";
   inps.setAttribute("placeholder", placeholder);
   return inps;
 }
@@ -23,6 +25,7 @@ divBox.appendChild(createInput("property", "property"));
 divBox.appendChild(createInput("value", "value"));
 
 //создаем функцию, которая добавляет кнопки...
+
 function createButton(className, nameFunction) {
   const btns = document.createElement("button");
   btns.setAttribute("class", className);
@@ -31,9 +34,10 @@ function createButton(className, nameFunction) {
 }
 
 //вызываем функцию...
-divBox.appendChild(createButton("btn", "createH1()")).textContent = "SaveH1";
-divBox.appendChild(createButton("btn", "createP()")).textContent = "SaveP";
-divBox.appendChild(createButton("btn", "createUl()")).textContent = "SaveUl";
+divBox.appendChild(createButton("btn", "createH1()")).textContent = "SAVE_H1";
+divBox.appendChild(createButton("btn", "createP()")).textContent = "SAVE_P";
+divBox.appendChild(createButton("btn", "createUlInput()")).textContent = "SAVE_Ul";
+divBox.appendChild(createButton("btn", "createLi()")).textContent = "SAVE_Li";
 
 //функция которая должна принимать свойства и значения из input и добавлять <h1></h1>...
 function createH1() {
@@ -45,7 +49,8 @@ function createH1() {
   }
   newH1.style[newObjEl.property] = newObjEl.value;
   newH1.innerText = newObjEl.text;
-  body.appendChild(newH1);
+  // body.appendChild(newH1);
+  divBox.insertAdjacentElement("afterend", newH1);
 }
 
 //функция которая должна принимать свойства и значения из input и добавлять <p></p>...
@@ -58,20 +63,29 @@ function createP() {
   }
   newP.style[newObjElp.property] = newObjElp.value;
   newP.innerText = newObjElp.text;
-  body.appendChild(newP);
+  // body.appendChild(newP);
+  divBox.insertAdjacentElement("afterend", newP);
 }
 
-//функция которая должна принимать свойства и значения из input и добавлять <ul><li></li></ul>...
+//функция которая должна добавлять 3 доп. инпута на страницу для <li></li>...
+function createUlInput() {
+  divBox.insertAdjacentHTML("beforeend",
+  `<input name="textLi" placeholder="text_Li" style="width: 350px; height: 25px;">
+  <input name="propertyLi" placeholder="property_Li" style="width: 350px; height: 25px;">
+  <input name="valueLi" placeholder="value_Li" style="width: 350px; height: 25px;">`)
+};
+
+//функция которая должна принимать свойства и значения из input и добавлять <li></li> в <ul></ul>...
 const newUl = document.createElement("ul");
-function createUl() {
-  newUl.classList.add("list");
+divBox.insertAdjacentElement("afterend", newUl);
+function createLi() {
+  newUl.classList.add("list_Li");
   const newLi = document.createElement("li");
   const newObjElList = {};
-  for (let inpList of divBox.children) {
-    newObjElList[inpList.name] = inpList.value;
+  for (let inpListLi of divBox.children) {
+    newObjElList[inpListLi.name] = inpListLi.value;
   }
-  newLi.style[newObjElList.property] = newObjElList.value;
-  newLi.innerText = newObjElList.text;
-  body.appendChild(newUl);
-  newUl.append(newLi);
+  newLi.style[newObjElList.propertyLi] = newObjElList.valueLi;
+  newLi.innerText = newObjElList.textLi;
+  newUl.appendChild(newLi);
 }
