753b2c28900f9a96aff9b9e8e5881e2cbfe87c50
sidardzmitry <dmx3214@mail.ru>
2021-12-20 16:43:17 +0300

    ввпвмык

---
+++ practice_JS/learn.js
@@ -0,0 +1,13 @@
+let str = "15 *25,5 - (10 / 2)";
+let operations = [];
+let result = 0;
+let i = 0;
+function calculator(str) {
+    str = str.split(" ").join("");
+    return str;
+
+
+
+
+};
+console.log(calculator(str));
