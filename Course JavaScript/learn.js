8c0596fb33bb7c14e7b3a629d0d66ff0bb00b174
sidardzmitry <dmx3214@mail.ru>
2022-02-04 11:30:10 +0300

    qwertyu

--- Course JavaScript/learn.js
+++ Course JavaScript/learn.js
@@ -1,2 +1,13 @@
+// let btns = document.querySelectorAll('button');
+
+// for(let i = 0; i < btns.length; i++) {
+//     let btn = btns[i];
+//     btn.addEventListener('click', () => {
+//         btn.innerText = i;
+//         console.log(i);
+//     });
+// };
+
+
 
 
